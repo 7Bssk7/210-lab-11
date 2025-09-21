23f190787ff9451bb39e37f353ccc02eb8ced33a
7Bssk7 <arhip.fin3@gmail.com>
2025-09-20 20:37:09 -0700

    Update

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -2,8 +2,18 @@
 // IDE used: Visual studio
 #include <iostream>
 #include <string>
+using namespace std;
+
+const int NUM_CARS = 3;
+
+struct Car{
+    string make, model;
+    int year, miles;
+};
 
 int main(){
+
+
 
     return 0;
 }
