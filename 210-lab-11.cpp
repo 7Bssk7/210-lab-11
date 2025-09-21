c0621fbb00c64ddc1af68bfb352b357a520f18d3
7Bssk7 <arhip.fin3@gmail.com>
2025-09-20 21:12:34 -0700

    Update

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -15,25 +15,25 @@ void inputCars(Car *);
 void outputCars(Car *);
 
 int main(){
-
     Car *list = new Car[NUM_CARS];
     
     for(int i = 0; i < NUM_CARS; ++i){
         inputCars(&list[i]);
     }
-
+    for(int i = 0; i < NUM_CARS; ++i){
+        outputCars(&list[i]);
+    }
 
 
     return 0;
 }
 
 void inputCars(Car *c){
-
-    static int num = 1;
+    static int num = 0;
     cout << "Enter information about car #" << num << endl;
-    cout << "Make: ";
+    cout << "Enter the vehicle manufacturer: ";
     getline(cin, c->make);
-    cout << "Model: ";
+    cout << "Enter the vehicle model: ";
     getline(cin, c->model);
     cout<< "Enter the year of manufacture: ";
     cin >> c->year;
@@ -41,5 +41,16 @@ void inputCars(Car *c){
     cin >> c->miles;
     cout << "\n";
     cin.ignore();
+    num++;
+}
 
+void outputCars(Car *c){
+    static int num = 1;
+    cout << "List of the cars" << endl;
+    cout<< "Car #" << num+1 << endl;
+    cout << "Make: " << c->make << endl;
+    cout << "Model: " << c->model << endl;
+    cout << "Year of manufacture: " << c-> year << endl;
+    cout << "Cars mileage: " << c->miles << endl;
+    num++;
 }
