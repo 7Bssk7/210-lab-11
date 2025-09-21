917a7ed5b5fddd70abb6ac6008c12b84adfa4a50
7Bssk7 <arhip.fin3@gmail.com>
2025-09-20 20:58:48 -0700

    Update

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -11,9 +11,35 @@ struct Car{
     int year, miles;
 };
 
+void inputCars(Car *);
+void outputCars(Car *);
+
 int main(){
+
+    Car *list = new Car[NUM_CARS];
+    
+    for(int i = 0; i < NUM_CARS; ++i){
+        inputCars(&list[i]);
+    }
 
 
 
     return 0;
+}
+
+void inputCars(Car *c){
+
+    static int num = 1;
+    cout << "Enter information about car #" << num << endl;
+    cout << "Make: ";
+    getline(cin, c->make);
+    cout << "Model: ";
+    getline(cin, c->model);
+    cout<< "Enter the year of manufacture: ";
+    cin >> c->year;
+    cout << "Enter a car mileage: ";
+    cin >> c->miles;
+    cout << "\n";
+    cin.ignore();
+
 }
